74d8a910b60c6afb735bddf7e5cca63423338399
VANSH MODI N. <154253890+ModiVanshN@users.noreply.github.com>
2025-07-15 14:06:14 +0530

    Update Api.js

--- frontend/src/contexts/Api.js
+++ frontend/src/contexts/Api.js
@@ -1,7 +1,7 @@
-import axios from "axios"
+import axios from "axios";
 
 const API = axios.create({
-  baseURL:"http://localhost:4000/api/ChatApp",
+  baseURL: "https://chat-app-znyk.onrender.com/api/ChatApp",
   withCredentials: true
 });
 
